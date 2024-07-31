6ca0843706aa99141af5c119f27454c0a92406c9
yash-r8 <yashaman.jnv@gamil.com>
2024-07-31 22:00:55 +0530

    update on home page

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -26,7 +26,7 @@ function HomeComponent() {
 
                 <div style={{ display: "flex", alignItems: "center" }}>
 
-                    <h2>Apna Video Call</h2>
+                    <h2>Socket Chat</h2>
                 </div>
 
                 <div style={{ display: "flex", alignItems: "center" }}>
@@ -54,7 +54,7 @@ function HomeComponent() {
             <div className="meetContainer">
                 <div className="leftPanel">
                     <div>
-                        <h2>Providing Quality Video Call Just Like Quality Education</h2>
+                        <h2>Socket Up For Quality Video Call</h2>
 
                         <div style={{ display: 'flex', gap: "10px" }}>
 
